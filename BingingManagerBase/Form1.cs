e69c1ef95be0e6dd7857bde5e9be232c2be8b945
qq33158 <80340308+qq33158@users.noreply.github.com>
2021-10-08 13:34:39 +0800

    update

---
+++ BingingManagerBase/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace BingingManagerBase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (SqlConnection cn = new SqlConnection())
+            {
+                cn.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;" + "AttachDbFilename=|DataDirectory|Northwind.mdf;" + "Integrated Security=True";
+                DataSet ds = new DataSet();
+                SqlDataAdapter daCategory = new SqlDataAdapter("SELECT * FROM 產品類別", cn);
+                daCategory.Fill(ds, "產品類別");
+                SqlDataAdapter daProduct = new SqlDataAdapter("SELECT * FROM 產品資料", cn);
+                daProduct.Fill(ds, "產品資料");
+                ds.Relations.Add("FK_產品資料_產品類別", ds.Tables["產品類別"].Columns["類別編號"],
+                                                         ds.Tables["產品資料"].Columns["類別編號"]);
+                dgvCategory.DataSource = ds;
+                dgvCategory.DataMember = "產品類別";
+                dgvCategory.Dock = DockStyle.Top;
+                dgvProduct.DataSource = ds;
+                dgvProduct.DataMember = "產品類別.FK_產品資料_產品類別";
+                dgvProduct.Dock = DockStyle.Fill;
+
+            }
+        }
+    }
+}
